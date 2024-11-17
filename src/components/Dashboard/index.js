4019ad42b69887c4fe97490d8098d86f9fc8d3e4
narendratnreddy <narendratnreddy@gmail.com>
2024-11-17 15:00:03 +0530

    Added Tic tac toe game

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, {lazy, Suspense } from "react";
 import "./dashboard.css";
-import Layout from "../Layout/index";
+import Layout from "../Layout";
 import BowlingGame from "./../../BowlingScoreboard/components/Game";
-import User from "./../../User/index";
+import User from "./../../User";
 import NoPage from "./../NoPage";
 import { Routes, Route } from "react-router-dom";
+const TicTacToeGame =  lazy(() => import("./../../TicTacToeGame"));
+
 
 const Dashboard = () => {
   return (
     <div>
       Welcome to UI world
-      <Layout />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Layout />
       <Routes>
         <Route path="bowling" element={<BowlingGame />}></Route>
         <Route path="user" element={<User />}></Route>
+        <Route path="tictactoe" element={<TicTacToeGame />}></Route>
         <Route path="*" element={<NoPage />}></Route>
       </Routes>
+      </Suspense>
     </div>
   );
 };
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -11,6 +11,9 @@ const Layout = () => {
         <li>
           <Link to="/user">User App</Link>
         </li>
+        <li>
+          <Link to="/tictactoe">Tic Tac Toe App</Link>
+        </li>
       </ul>
       <Outlet />
     </div>
